784dd08d6a6f45b5cdfc7336ef4e92449e2531d5
hernanhrndz <hernanhg10@gmail.com>
2025-01-02 12:31:58 -0600

    accordian js file refactor

--- src/components/Accordian.js
+++ src/components/Accordian.js
@@ -6,172 +6,30 @@ import clay from "../assets/claymation.png";
 import pomade from "../assets/suavecito-pomade.png";
 import powder from "../assets/suavecito-texture-powder.png";
 
+const accordionData = [
+    { id: 1, imgSrc: stylingPowder, name: "placeholder", description: "info info info" },
+    { id: 2, imgSrc: pomade, name: "placeholder", description: "info info info" },
+    { id: 3, imgSrc: clay, name: "placeholder", description: "info info info" },
+    { id: 4, imgSrc: powder, name: "placeholder", description: "info info info" },
+
+]
+
 export const Accordian = () => {
     return (
-        <ul className="accordion">
-        <li>
-            <img src={stylingPowder} />
-            <div class="content">
-            <span>
-                <h2>Carmen Rios</h2>
-                <p>Frontend</p>
-            </span>
+        <div className="accordion">
+        {accordionData.map((item) => (
+          <div className="accordion-item" key={item.id}>
+            <img src={item.imgSrc} alt={`${item.name}`} />
+            <div className="content">
+              <span>
+                <h2>{item.name}</h2>
+                <p>{item.description}</p>
+              </span>
             </div>
-        </li>
-        <li>
-            <img src={pomade} />
-            <div class="content">
-            <span>
-                <h2>Lisa Scott</h2>
-                <p>Backend</p>
-            </span>
-            </div>
-        </li>
-        <li>
-            <img src={clay}/>
-            <div class="content">
-            <span>
-                <h2>Pavel Dvorak</h2>
-                <p>Backend</p>
-            </span>
-            </div>
-        </li>
-        <li>
-            <img src={powder} />
-            <div class="content">
-            <span>
-                <h2>Kelly Cox</h2>
-                <p>Designer</p>
-            </span>
-            </div>
-        </li>
-    </ul>
-
+          </div>
+        ))}
+      </div>
     );
 };
 
 export default Accordian;
-
-
-{/* <div className="accordion-panel">
-            <h2 id="panel1-heading">
-                <button
-                    className="accordion-trigger"
-                    aria-controls="panel1-content"
-                    aria-expanded="true"
-                > 
-                    <span id="panel1-title">Texture Powder </span>
-                    
-                </button>
-            </h2>
-
-            <div 
-                className="accordian-content" 
-                id="panel1-content"
-                aria-labelledby="panel1-heading"
-                aria-hidden="false"
-                role="region"
-                >
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur
-                </p> 
-                <img
-                    className="accordion-image"
-                    src={stylingPowder}
-                    alt="level3 Styling Powder"
-                /> 
-            </div>
-        </div> */}
-
-        {/* second according panel */}
-        {/* <div className="accordion-panel">
-            <h2 id="panel2-heading">
-                <button
-                    className="accordion-trigger"
-                    aria-controls="panel2-content"
-                    aria-expanded="false"
-                > 
-                    <span id="panel2-title">Clay </span>
-                    
-                </button>
-            </h2>
-
-            <div 
-                className="accordian-content" 
-                id="panel2-content"
-                aria-labelledby="panel2-heading"
-                aria-hidden="true"
-                role="region"
-                >
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur
-                </p> 
-                <img
-                    className="accordion-image"
-                    src={clay}
-                    alt="Hanz de fuko claymation"
-                /> 
-            </div>
-        </div> */}
-        
-        {/* Third according panel */}
-        {/* <div className="accordion-panel">
-            <h2 id="panel3-heading">
-                <button
-                    className="accordion-trigger"
-                    aria-controls="panel3-content"
-                    aria-expanded="false"
-                > 
-                    
-                </button>
-            </h2>
-
-            <div 
-                className="accordian-content" 
-                id="panel3-content"
-                aria-labelledby="panel3-heading"
-                aria-hidden="true"
-                role="region"
-                >
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur
-                </p> 
-                <img
-                    className="accordion-image"
-                    src={powder}
-                    alt="Suavecito powder"
-                /> 
-            </div>
-        </div> */}
-
-
-        {/* Fourth according panel */}
-        {/* <div className="accordion-panel">
-            <h2 id="panel4-heading">
-                <button
-                    className="accordion-trigger"
-                    aria-controls="panel4-content"
-                    aria-expanded="false"
-                > 
-                    <span id="panel4-title">Pomade </span>
-            
-                </button>
-            </h2>
-
-            <div 
-                className="accordian-content" 
-                id="panel4-content"
-                aria-labelledby="panel4-heading"
-                aria-hidden="true"
-                role="region"
-                >
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur
-                </p> 
-                <img
-                    className="accordion-image"
-                    src={pomade}
-                    alt="pomade"
-                /> 
-            </div>
-        </div> */}
