afcc8180e6aa02ef2cd402985daffb0094541e0d
hernanhrndz <hernanhg10@gmail.com>
2025-01-11 22:54:58 -0800

    Banner css update for img/title/button

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,11 +6,14 @@ export const Banner = () => {
     return (
         <main>
             <section className="banner-section padding-block-900">
-                <div className='container'>
-                    <div className='two-column'>
-                        <div className='flow banner-content'>
+                <div className="container">
+                    <div className="two-column">
+                        <div className="flow banner-content">
                             <h1 className="fw-bold fs-headings ">Blended Blessed for all your needs</h1>
                             <button className="booking-btn"> Call Now! </button>
+                        </div>
+                        <div className="image-background">
+                            <img src={sampleImg} alt="barbershop-img" className="banner-image" />
                         </div>
                     </div>
                 </div>
